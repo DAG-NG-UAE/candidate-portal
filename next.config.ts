7c3b8cafe51ed71501c43c47e7a6084938973897
Isabella Kpai <isabella.k@bajajnigeria.com>
2026-03-17 14:33:46 +0100

    merge conflict

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', 
+  output: 'export', // <--- Add this line
   images: {
-    unoptimized: true,
+    unoptimized: true, // Static exports don't support Next.js Image Optimization
   },
-
 };
 
 export default nextConfig;
